6c144c79b3a645693b270850af0c1d41beb758d6
CodeLikePanda <jayeshnrsarkar7mn@gmail.com>
2024-11-17 16:36:57 +0530

    Initial push

--- scripts/server.js
+++ scripts/server.js
@@ -21,13 +21,13 @@ const server_mode = args.mode === "production" ? SERVER_MODES.PRODUCTION : SERVE
 
 if (server_mode === SERVER_MODES.DEVELOPMENT) {
     let startFrontendDevServer = (await import("./server-frontend-dev.js")).startFrontendDevServer;
-    console.info("Starting server in development mode.");
+    console.info("Starting server at https://cloud13.de/testwhiteboard/");
     startFrontendDevServer(8080, function () {
         // this time, it's the frontend server that is on port 8080
         // requests for the backend will be proxied to prevent cross origins errors
         startBackendServer(3000);
     });
 } else {
-    console.info("Starting server in production mode.");
+    console.info("Starting server at https://cloud13.de/testwhiteboard/");
     startBackendServer(process.env.PORT || 8080);
 }
